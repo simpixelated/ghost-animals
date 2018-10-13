2bcbdad63b5c8279bed8be385238c58a0d6d06c6
Jordan Kohl <kohl.jordan@gmail.com>
2018-10-13 14:32:32 -0700

    cleanup express app and readme

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-A really simple frontend + backend JavaScript application: The backend server (Express or whatever you prefer) includes a route that returns a list of animals in JSON format. The frontend app uses React. It simply places a button on the page, and when that button is clicked, the app pulls down the list of animals from the backend and renders it in a table.
+A really simple frontend + backend JavaScript application: The backend server (Express) includes a route that returns a list of animals in JSON format. The frontend app uses React. It simply places a button on the page, and when that button is clicked, the app pulls down the list of animals from the backend and renders it in a table.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Start the app:
+Run locally:
 ```
 npm install
 npm start
 ```
+
+Or [![Deploy to
+Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/simpixelated/ghost-animals)
--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,16 @@
 const serverless = require('serverless-http')
-// const functionName = 'serverless-http'
 const express = require('express');
 
-const expressApp = (functionName) => {
+const expressApp = () => {
   const app = express();
-  const basePath = (process.env.NODE_ENV === 'development') ? `/` : `/.netlify/functions/index`;
-  app.set("port", process.env.PORT || 3002);
+  const basePath = (process.env.NODE_ENV === 'development') ? '/' : '/.netlify/functions/index';
+  app.set('port', process.env.PORT || 3002);
   app.use(basePath, (req, res, next) => {
-    res.header("Content-Type",'application/json');
+    res.header('Content-Type','application/json');
 
     // TODO: disable if no longer developing
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     next();
   });
@@ -36,13 +35,11 @@ const expressApp = (functionName) => {
   })
 
   app.listen(app.get("port"), () => {
-    console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
   });
 
   return app;
 };
 
-const app = expressApp()
-
 // Export lambda handler
-exports.handler = serverless(app)
+exports.handler = serverless(expressApp());
