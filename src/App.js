419e65a6ffc6fa32882b9502c75fb390ab4737cc
Jordan Kohl <kohl.jordan@gmail.com>
2018-10-14 09:06:44 -0700

    added loading and loaded states

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 
 class App extends Component {
   state = {
-    animals: []
+    animals: [],
+    loading: false
   }
 
   handleClickGetAnimals = () => {
     // no need to fetch if animals are already set
     // TODO: be smarter about caching; e.g. fetch if older than a minute
     if (!this.state.animals.length) {
+      this.setState({ loading: true });
       this.fetchAnimals()
-        .then(animals => this.setState({ animals }));
+        .then(animals => {
+          // display "loading" for a minimum amount of time to prevent flicker on fast connections
+          setTimeout(() => this.setState({ animals, loading: false }), 500);
+        });
     }
   }
   
@@ -23,13 +28,15 @@ class App extends Component {
   }
 
   render() {
+    const isButtonDisabled = this.state.loading || this.state.animals.length;
     return (
       <div className="App">
         <header className="App-header">
           <h1>Ghost Animals</h1>
         </header>
         <div>
-          <p><Button onClick={this.handleClickGetAnimals}>Get spooky animals!</Button></p>
+          <p><Button onClick={this.handleClickGetAnimals} disabled={isButtonDisabled}>Get spooky animals!</Button></p>
+          {this.state.loading && <p>Loading...</p>}
           <AnimalList animals={this.state.animals} />
         </div>
       </div>
